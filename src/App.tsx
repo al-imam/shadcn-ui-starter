2cd4586837d7cfcfa0631b383d9c19a85bc3fbbd
al-imam <alimam01828@gmail.com>
2023-09-09 12:52:58 +0600

    setup routes flow and protect routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "@context/auth-context";
+import { Private } from "@routes/private";
+import { Public } from "@routes/public";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <ThemeProvider defaultTheme="light">Hello</ThemeProvider>
+        <ThemeProvider defaultTheme="light">
+          <Routes>
+            <Route path="/agent" element={<Private page="agent" to="/auth/agent/login" />}>
+              <Route index element={<h1>You're in agent index page</h1>} />
+              <Route path="*" element={<Navigate to="/agent" />} />
+            </Route>
+            <Route path="/admin" element={<Private page="admin" to="/auth/admin/login" />}>
+              <Route index element={<h1>You're in admin index page</h1>} />
+              <Route path="*" element={<Navigate to="/admin" />} />
+            </Route>
+            <Route path="/auth">
+              <Route path="agent" element={<Public page="agent" to="/auth/agent/login" />}>
+                <Route path="login" element={<h1>You're in auth/agent/login index page</h1>} />
+                <Route path="registration" element={<h1>You're in auth/agent/registration index page</h1>} />
+                <Route index path="*" element={<Navigate to="/auth/agent/login" />} />
+              </Route>
+              <Route path="admin" element={<Public page="admin" to="/admin" />}>
+                <Route path="login" element={<h1>You're in auth/agent/login index page</h1>} />
+                <Route path="registration" element={<h1>You're in auth/agent/registration index page</h1>} />
+                <Route index path="*" element={<Navigate to="/auth/admin/login" />} />
+              </Route>
+            </Route>
+          </Routes>
+        </ThemeProvider>
       </AuthProvider>
     </BrowserRouter>
   );
