d4be164ff576bd75b61b06a68f8c837420461cfa
al-imam <alimam01828@gmail.com>
2023-09-09 10:23:10 +0600

    setup theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { ThemeProvider, useTheme } from "@/components/theme-provider";
 
 function App() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <ThemeProvider defaultTheme="light">
+      <div>
+        <MyButton />
+      </div>
+    </ThemeProvider>
   );
+}
+
+function MyButton() {
+  const { setTheme, theme } = useTheme();
+  return <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>{theme}</Button>;
 }
 
 export default App;
