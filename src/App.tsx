a6340aa32845997e7a5180a056af4465e050bb2e
al-imam <alimam01828@gmail.com>
2023-09-09 10:10:51 +0600

    setup shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
